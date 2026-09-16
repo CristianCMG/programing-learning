95eb4d4e38b665a5cbdf3bdb559cb267abf49a6b
Cristian <cristianguala2003@gmail.com>
2026-09-16 02:44:54 -0300

    initial commit

---
+++ axe_game.cpp
@@ -0,0 +1,12 @@
+#include "raylib.h"
+
+int main()
+{ 
+    float root_beer = 1.99;
+    double cheese_burger = 5.99;
+    bool shouldHaveLunch = true;
+    int width = 1440;
+    int height = 810;
+    InitWindow(width, height, "axe game");
+   
+}
