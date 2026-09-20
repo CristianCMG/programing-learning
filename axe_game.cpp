a0483c5d5edf947e41f8e105f9670d6f27d0143c
Cristian <cristianguala2003@gmail.com>
2026-09-20 04:59:51 -0300

    bools

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,8 +3,10 @@
 int main()
 { 
     float root_beer = 1.99;
-    double cheese_burger = 5.99;
-    bool shouldHaveLunch = true;
+    double cheese_burger{5,99}; 
+    bool shouldHaveLunch{true};
+    bool equal{ 4 == 9}
+    
     int width = 1440;
     int height = 810;
     InitWindow(width, height, "axe game");
